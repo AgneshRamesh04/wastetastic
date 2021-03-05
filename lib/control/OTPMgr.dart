3c6e995958404cb56119aa8c8cefe27700709b0b
khushk21 <56465845+khushk21@users.noreply.github.com>
2021-03-05 22:50:24 +0800

    added OTP manager and Registration manager classes

---
+++ lib/control/OTPMgr.dart
@@ -0,0 +1,21 @@
+import 'package:email_auth/email_auth.dart';
+class OTPMgr {
+  static void sendOTP(String email) async {
+    EmailAuth.sessionName = "Verification";
+    var response = await EmailAuth.sendOtp(receiverMail: email);
+    if (response) {
+      print("OTP SENT");
+    } else {
+      print("problem encountered");
+    }
+  }
+
+  static bool verifyOTP(String email, String otp) {
+    var response = EmailAuth.validate(receiverMail: email, userOTP: otp);
+    if (response) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
---
+++ lib/control/RegistrationMgr.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:wastetastic/entity/UserAccount.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'OTPMgr.dart';
+
+class RegistrationMgr {
+  int num_attempts;
+  String otp;
+
+  RegistrationMgr(int num_attempts) {
+    num_attempts = num_attempts;
+  }
+
+  final _firestore = FirebaseFirestore.instance;
+  RegistrationMgr r = RegistrationMgr(1);
+
+  void incrementNumAttempts() {
+    r.num_attempts++;
+  }
+
+  void registerUserAccount(
+      String name, String email, String password, String username) async {
+    final auth = FirebaseAuth.instance;
+
+    try {
+      final newUser = await auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      if (newUser != null) {
+        String userEmail = newUser.user.email;
+
+        while (r.num_attempts != 3) {
+          OTPMgr.sendOTP(userEmail);
+          bool result = OTPMgr.verifyOTP(userEmail, r.otp);
+          if (result) {
+            print("verified");
+            _firestore
+                .collection('UserAccounts')
+                .doc('$username')
+                .set({'name': name, 'email': userEmail, 'password': password});
+            //add navigation here to the home screen
+            break;
+          } else {
+            incrementNumAttempts();
+          }
+          if (num_attempts == 3) {
+            print("failed");
+            //Navigate back to the signup page.
+          }
+        }
+      }
+    } catch (e) {
+      // need to display this error onto screen instead of
+      print(e);
+    }
+  }
+}
