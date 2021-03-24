9425f792c6f5625601d3a6c06b03e21da27b4f76
Rachita <58717131+rachita7@users.noreply.github.com>
2021-03-24 12:07:56 +0800

    Add files via upload

--- lib/widgets/HeaderCard.dart
+++ lib/widgets/HeaderCard.dart
@@ -11,13 +11,15 @@ class header_card extends StatelessWidget {
       children: [
         Padding(
           padding: const EdgeInsets.only(
-            top: 20.0,
+            top: 30.0,
             bottom: 10.0,
           ),
           child: Text(
             title,
-            style: TextStyle(fontSize: 20),
+            style: TextStyle(
+            fontSize: 30.0, fontFamily: "Source Sans Pro", color: Colors.teal[900]),
             overflow: TextOverflow.ellipsis,
+
           ),
         ),
         Divider(
@@ -25,6 +27,7 @@ class header_card extends StatelessWidget {
           thickness: 5.0,
           indent: 100.0,
           endIndent: 100.0,
+          color: Colors.lime,
         ),
         SizedBox(
           height: 20.0,
--- lib/widgets/POICard.dart
+++ lib/widgets/POICard.dart
@@ -11,6 +11,7 @@ class POI_card extends StatelessWidget {
   final bool fav;
   final Function FavFunct;
 
+
   POI_card(
       {this.name,
       this.address,
@@ -23,28 +24,42 @@ class POI_card extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String cat= wasteCategory.toLowerCase();
+    String wasteCat= cat[0].toUpperCase()+cat.substring(1);
     return RawMaterialButton(
+
       onPressed: TO_POI_page,
       child: Container(
         padding: EdgeInsets.all(20.0),
         margin: EdgeInsets.all(10.0),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(10.0),
-          color: Colors.grey[900],
+          //color: Colors.teal[900],
+          gradient: LinearGradient(
+            //begin: Alignment.topRight,
+            //end: Alignment.bottomLeft,
+            colors: [
+              Colors.lime.shade400,
+              Colors.lightGreen.shade400,
+            ],
+          ),
           boxShadow: kContainerElevation,
         ),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Flexible(
+
               child: Text(
-                "$name\n$address. Singapore $postalcode "
-                "\nCategory: $wasteCategory \n$description",
+                "$name\n$address\nPostal Code: $postalcode "
+                "\nCategory: $wasteCat",
                 overflow: TextOverflow.fade,
                 softWrap: false,
                 style: TextStyle(
+                  //fontFamily: "Source Sans Pro",
+                  color: Colors.teal[900],
                   height: 1.5,
-                  letterSpacing: 1,
+                  letterSpacing: 0.8,
                 ),
               ),
             ),
@@ -57,7 +72,7 @@ class POI_card extends StatelessWidget {
               onPressed: FavFunct,
               icon: Icon(
                 Icons.star,
-                color: fav ? Colors.yellow : Colors.white,
+                color: fav ? Colors.yellow.shade600 : Colors.white,
               ),
             ),
           ],
